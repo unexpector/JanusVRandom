5081ccbbacb018d74c857452cfe148cf2c34132f
unexpector <stevemills78@gmail.com>
2015-07-03 12:08:50 +1000

    retry

--- hello/views.py
+++ hello/views.py
@@ -5,7 +5,13 @@ from .models import Greeting
 
 # Create your views here.
 def index(request):
-    return HttpResponse('Hello from Python! Now Changed')
+
+    greeting = Greeting()
+    greeting.save()
+
+    greetings = Greeting.objects.all()
+
+    return render(request, 'unexpector-template.html', {'greetings': greetings})
 
 
 def db(request):
