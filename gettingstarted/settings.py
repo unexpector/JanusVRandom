7953197b17a864f28a88e76a147c075fccf3b482
unexpector <stevemills78@gmail.com>
2015-07-03 23:24:14 +1000

    settings file for media

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -100,6 +100,10 @@ BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
 
+
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media') # Absolute path to the media directory
