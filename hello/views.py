d6f203b8817c9e6c92146c56f934aa6b98d4b283
unexpector <stevemills78@gmail.com>
2015-07-03 11:57:25 +1000

    VR Room Template
    VR Room Template

--- hello/views.py
+++ hello/views.py
@@ -5,7 +5,12 @@ from .models import Greeting
 
 # Create your views here.
 def index(request):
-    return HttpResponse('Hello from Python! Now Changed')
+     greeting = Greeting()
+    greeting.save()
+
+    greetings = Greeting.objects.all()
+
+    return render(request, 'unexpector-template.html', {'greetings': greetings})
 
 
 def db(request):
