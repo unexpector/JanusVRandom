9538b998ad17bfbc4ee20c8a3afdd6cd70728044
unexpector <stevemills78@gmail.com>
2015-07-03 12:21:37 +1000

    rollback - db

--- hello/views.py
+++ hello/views.py
@@ -1,21 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.contrib.auth import authenticate, logout
-from django.contrib.auth import login as auth_login
-from django.shortcuts import render_to_response
-from django.template import RequestContext
-from .models import Greeting
 
+from .models import Greeting
 
 # Create your views here.
 def index(request):
-
-    greeting = Greeting()
-    greeting.save()
-
-    greetings = Greeting.objects.all()
-
-    return render(request, 'unexpector-template.html', {'greetings': greetings})
+    return HttpResponse('Hello from Python! Now Changed')
 
 
 def db(request):
@@ -25,5 +15,5 @@ def db(request):
 
     greetings = Greeting.objects.all()
 
-    return render(request, 'db.html', {'greetings': greetings})
+    return render(request, 'unexpector-template.html', {'greetings': greetings})
 
