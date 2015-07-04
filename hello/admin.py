8e5e10bdf72180224e459cbf7ba87ea08e72d8ff
unexpector <stevemills78@gmail.com>
2015-07-04 19:03:32 +1000

    Register Models in Admin

--- hello/admin.py
+++ hello/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from hello.models import Rooms, RandomSites, ObjectLibrary
 
 # Register your models here.
+admin.site.register(Rooms)
+admin.site.register(RandomSites)
+admin.site.register(ObjectLibrary)
