2d2b11b2ed8dc4b6e5579494340f7eb4fd287adb
unexpector <stevemills78@gmail.com>
2015-07-05 00:10:59 +1000

    admin fix

--- hello/admin.py
+++ hello/admin.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 from hello.models import Rooms, RandomSites, ObjectLibrary
 
-# Register your models here.
-admin.site.register(Rooms)
-admin.site.register(RandomSites)
-admin.site.register(ObjectLibrary)
-
 class RandomSitesAdmin(admin.ModelAdmin):
     list_display = ('sitename', 'src')
+
+# Register your models here.
+admin.site.register(Rooms)
+admin.site.register(RandomSites, RandomSitesAdmin)
+admin.site.register(ObjectLibrary)
+
