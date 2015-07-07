3c04dd91b4f15ac47f513269a99a25659faa0324
unexpector <stevemills78@gmail.com>
2015-07-07 11:58:06 +1000

    select 1 record

--- hello/views.py
+++ hello/views.py
@@ -6,7 +6,8 @@ from hello.models import RandomSites, ObjectLibrary, Rooms
 # Create your views here.
 def index(request):
     newvalue = "new value"
-    random_list = RandomSites.objects.all().order_by('src')
+    namepicked = "Twitter"
+    random_list = RandomSites.objects.all().filter(sitename=namepicked).order_by('src')
     context_dict = {'boldmessage': random_list}
 
     return render(request, 'unexpector-template.html', context_dict)
