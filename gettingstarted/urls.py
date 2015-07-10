52575feb78f2bf967b8b474821e9f596074c4f26
unexpector <stevemills78@gmail.com>
2015-07-10 15:09:01 +1000

    try

--- gettingstarted/urls.py
+++ gettingstarted/urls.py
@@ -15,7 +15,7 @@ urlpatterns = patterns('',
     url(r'^db', hello.views.db, name='db'),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^about', hello.views.about, name='about'),
-    url(r'^slugpage/(?P<category_name_slug>[\w\-]+)/$', hello.views.about, name='slugpage'),
+    url(r'^slugpage', hello.views.about, name='slugpage'),
 
 
 )
