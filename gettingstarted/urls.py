3972efdda81606b83871fe7a520f13552623b0bd
unexpector <stevemills78@gmail.com>
2015-07-10 15:48:44 +1000

    add trail slash

--- gettingstarted/urls.py
+++ gettingstarted/urls.py
@@ -15,7 +15,7 @@ urlpatterns = patterns('',
     url(r'^db', hello.views.db, name='db'),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^about', hello.views.about, name='about'),
-    url(r'^slugpage(?P<site_name_slug>[\w\-]+)/', hello.views.category, name='slugpage'),
+    url(r'^slugpage/(?P<site_name_slug>[\w\-]+)/', hello.views.category, name='slugpage'),
 
 
 )
