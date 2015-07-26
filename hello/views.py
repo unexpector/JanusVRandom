673aeb566f42aea4662ff62a5d052aa67534cf81
unexpector <stevemills78@gmail.com>
2015-07-26 16:28:11 +1000

    extra

--- hello/views.py
+++ hello/views.py
@@ -39,26 +39,6 @@ def index(request):
     context_dict['page_name'] = "this is the page name"
     return render(request, 'unexpector-template.html', context_dict)
 
-class item(object):
-    def createitem(self):
-        setx = randint(-2, 5)
-        sety = 1
-        setz = randint(-10,-1)
-        objlocation = [setx,sety,setz]
-        #Set Dimensions
-        objx = objlocation[0]
-        objy = objlocation[1]
-        objz = objlocation[2]
-
-def newobject(name)
-    setx = randint(-2, 5)
-    sety = 1
-    setz = randint(-10,-1)
-    objlocation = [setx,sety,setz]
-    #Set Dimensions
-    objx = objlocation[0]
-    objy = objlocation[1]
-    objz = objlocation[2]
 
 def db(request):
 
