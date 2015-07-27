662e6b432ed03a279e2a7bb4b706df94edec5c29
unexpector <stevemills78@gmail.com>
2015-07-27 13:08:56 +1000

    commit object creation

--- hello/utils.py
+++ hello/utils.py
@@ -1,15 +1,12 @@
 from random import randint
 
-class item(object):
-    def createitem(self):
-        setx = randint(-2, 5)
-        sety = 1
-        setz = randint(-10,-1)
-        objlocation = [setx,sety,setz]
-        #Set Dimensions
-        objx = objlocation[0]
-        objy = objlocation[1]
-        objz = objlocation[2]
+class createobject(object):
+    def __init__(self, name):
+        self.name = name
+        self.position = []    # creates a new location list
+
+    def add_co_ords(self, location):
+        self.position.append(location)
 
 def newobject():
     setx = randint(-2, 5)
